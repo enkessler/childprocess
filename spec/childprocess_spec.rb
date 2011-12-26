0f362b66f3a34075b2153fb340106611a987b1a2
Jari Bakken <jari.bakken@gmail.com>
2011-12-27 00:57:10 +0100

    Switch back to poll_for_exit for the duplex spec.
    There's apparently a problem with writing to stdin on both JRuby and Windows.

--- spec/childprocess_spec.rb
+++ spec/childprocess_spec.rb
@@ -165,7 +165,7 @@ describe ChildProcess do
       process.io.stdin.puts "hello world"
       process.io.stdin.close
 
-      process.wait
+      process.poll_for_exit(POLL_FOR_EXIT)
 
       out.rewind
       out.read.should == "hello world\n"
