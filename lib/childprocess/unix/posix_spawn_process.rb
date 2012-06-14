281ceff8793f32ef70c1f73df76c7f3cf0990c6a
Chris Schmich <schmch@gmail.com>
2012-06-13 20:34:43 -0700

    Adding cwd (current working directory) support to Posix-spawned processes. You can now set the current working directory for a process before it starts. Still needs formal tests.
    Unfortunately, because posix_spawnp does not take any argument to change the child's current working directory, we have to change the parent process' directory so that it gets inherited. Additionally, since the current directory is a process-wide setting and not per-thread, this opens up a small window for race conditions since the chdir might conflict with other directory changes in other threads. If this is an issue, the guidance would be to stick with fork/exec.

--- lib/childprocess/unix/posix_spawn_process.rb
+++ lib/childprocess/unix/posix_spawn_process.rb
@@ -41,21 +41,32 @@ module ChildProcess
         argv = Argv.new(@args)
         envp = Envp.new(ENV.to_hash.merge(@environment))
 
-        if ChildProcess.jruby?
-          # on JRuby, the current working directory is for some reason not inherited.
-          # We'll work around it by making a chdir call through FFI.
-          # TODO: report this to JRuby
-          Lib.chdir Dir.pwd
+        if !@cwd.nil?
+          orig_pwd = Dir.pwd
+          Lib.chdir @cwd
         end
 
-        ret = Lib.posix_spawnp(
-          pid_ptr,
-          @args.first, # TODO: not sure this matches exec() behaviour
-          actions,
-          attrs,
-          argv,
-          envp
-        )
+        begin
+          if ChildProcess.jruby?
+            # on JRuby, the current working directory is for some reason not inherited.
+            # We'll work around it by making a chdir call through FFI.
+            # TODO: report this to JRuby
+            Lib.chdir Dir.pwd
+          end
+
+          ret = Lib.posix_spawnp(
+            pid_ptr,
+            @args.first, # TODO: not sure this matches exec() behaviour
+            actions,
+            attrs,
+            argv,
+            envp
+          )
+        ensure
+          if !orig_pwd.nil?
+            Lib.chdir(orig_pwd)
+          end
+        end
 
         if duplex?
           io._stdin = writer
