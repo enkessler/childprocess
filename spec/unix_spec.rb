0be34d903a5432ab87a6883adc8cba53b797bc52
Jari Bakken <jari.bakken@gmail.com>
2012-07-20 12:33:07 +0200

    Fix spec for rbx

--- spec/unix_spec.rb
+++ spec/unix_spec.rb
@@ -12,7 +12,7 @@ if ChildProcess.unix? && !ChildProcess.jruby? && !ChildProcess.posix_spawn?
       process.stub!(:fork).and_return('fakepid')
       process.stub!(:send_term)
       process.stub!(:poll_for_exit).and_raise(ChildProcess::TimeoutError)
-      process.stub!(:send_kill).and_raise(Errno::ECHILD)
+      process.stub!(:send_kill).and_raise(Errno::ECHILD.new)
 
       process.start
       lambda { process.stop }.should_not raise_error
@@ -26,7 +26,7 @@ if ChildProcess.unix? && !ChildProcess.jruby? && !ChildProcess.posix_spawn?
       process.stub!(:fork).and_return('fakepid')
       process.stub!(:send_term)
       process.stub!(:poll_for_exit).and_raise(ChildProcess::TimeoutError)
-      process.stub!(:send_kill).and_raise(Errno::ESRCH)
+      process.stub!(:send_kill).and_raise(Errno::ESRCH.new)
 
       process.start
       lambda { process.stop }.should_not raise_error
