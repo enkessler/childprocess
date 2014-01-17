4f10f698b61e69481729711a9418749285204f9a
Jari Bakken <jari.bakken@gmail.com>
2014-01-17 16:42:01 +0100

    No coveralls on JRuby, apparently

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'coveralls'
-Coveralls.wear!
+unless defined?(JRUBY_VERSION)
+  require 'coveralls'
+  Coveralls.wear!
+end
 
 require 'childprocess'
 require 'rspec'
