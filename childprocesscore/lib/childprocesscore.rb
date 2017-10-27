8925c45abe309b9db31b499d55376aa8e942586f
David Schmitt <david.schmitt@puppet.com>
2017-10-27 09:34:27 +0100

    Make posix_spawn only writable on main

--- childprocesscore/lib/childprocesscore.rb
+++ childprocesscore/lib/childprocesscore.rb
@@ -89,14 +89,6 @@ module ChildProcess
       false
     end
 
-    #
-    # Set this to true to enable experimental use of posix_spawn.
-    #
-
-    def posix_spawn=(bool)
-      @posix_spawn = bool
-    end
-
     def os
       @os ||= (
         require "rbconfig"
