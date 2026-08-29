cb336515608160def7be2730efa03ddceecbb0ce
Songstats Audit <audit@songstats.local>
2026-08-29 22:20:42 +0200

    Do not treat stopped children as exited

--- lib/childprocess/process_spawn_process.rb
+++ lib/childprocess/process_spawn_process.rb
@@ -8,7 +8,7 @@ module ChildProcess
       return true if @exit_code
 
       assert_started
-      pid, status = ::Process.waitpid2(@pid, ::Process::WNOHANG | ::Process::WUNTRACED)
+      pid, status = ::Process.waitpid2(@pid, ::Process::WNOHANG)
       pid = nil if pid == 0 # may happen on jruby
 
       log(:pid => pid, :status => status)
